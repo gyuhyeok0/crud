848470b893542d944b61c23e32e0880bb69ff92b
pyunghun <564564q@naver.com>
2024-03-12 14:24:05 +0900

    123

---
+++ src/main/java/com/ohgiraffers/section01/xmlconfig/MenuService.java
@@ -0,0 +1,47 @@
+package com.ohgiraffers.section01.xmlconfig;
+
+/* 필기.
+ *   Service 의 역할
+ *   1. SqlSession 생성
+ *   2. DAO(데이터베이스 접근 객체)의 메소드 호출
+ *   3. 트랜젝션(commit,rollback) 제어
+ *   4. SqlSession 닫기
+ *  */
+
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
+import static com.ohgiraffers.section01.xmlconfig.Template.getSqlSession;
+
+public class MenuService {
+
+    private final MenuDAO menuDAO;
+
+    public MenuService() {
+        this.menuDAO = new MenuDAO();
+    }
+
+    public List<MenuDTO> selectAllMenu() {
+
+        SqlSession sqlSession = getSqlSession();
+
+        List<MenuDTO> menuList = menuDAO.selectAllMenu(sqlSession); //
+
+        sqlSession.close();
+
+        return menuList;
+
+    }
+
+    public MenuDTO selectMenuByCode(int code) {
+
+        SqlSession sqlSession = getSqlSession();
+
+        MenuDTO menu = menuDAO.selectMenuByCode(sqlSession, code);
+
+        sqlSession.close();
+
+        return menu;
+    }
+}
---
+++ src/main/java/com/ohgiraffers/section01/xmlconfig/PrintResult.java
@@ -0,0 +1,42 @@
+package com.ohgiraffers.section01.xmlconfig;
+
+import java.util.List;
+
+public class PrintResult {
+
+    /* 필기.
+        사용자-> 인포직원 -> 청소업체
+        요청의 결과 <-   <-
+     */
+
+
+    public void printMenuList(List<MenuDTO> menuList) {
+
+        System.out.println("전체 메뉴 조회의 결과는!!!");
+        for(MenuDTO menu : menuList) {
+
+            System.out.println(menu);
+        }
+
+    }
+
+
+    public void printErrorMessage(String errorCode) {
+
+        String errorMessage = "";
+        switch (errorCode) {
+            case "selectList" : errorMessage = "메뉴조회를 실패했습니다..."; break;
+            case "selectOne" : errorMessage = "메뉴 코드로 메뉴 조회를 실패했습니다."; break;
+        }
+
+        System.out.println(errorMessage);
+    }
+
+    public void printMenu(MenuDTO menu) {
+
+        System.out.println(menu);
+    }
+
+
+
+}
